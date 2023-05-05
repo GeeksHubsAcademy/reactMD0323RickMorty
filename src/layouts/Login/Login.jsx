42cf23580be11cf32c2b7cbb2b70aea2d8dc6f5f
Dave86Dev <davidochando1986@gmail.com>
2023-05-05 13:56:18 +0200

    Basic binding on login layout

--- src/layouts/Login/Login.jsx
+++ src/layouts/Login/Login.jsx
@@ -1,13 +1,49 @@
 
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Login.css';
+import { InputText } from '../../components/InputText/InputText';
 
 export const Login = () => {
 
+    //Hook
+    const [credentials, setCredentials] = useState({
+        email: "",
+        password: ""
+    })
+
+    const inputHandlerFunction =(e)=>{
+        
+        setCredentials((prevState)=>({
+          ...prevState,
+          [e.target.name]:e.target.value
+        }))
+
+    }
+
+    useEffect(()=>{
+
+        console.log(credentials);
+
+    },[credentials]);
+
     return(
         <div className='loginDesign'>
-            soy Login
+            <InputText 
+                type={"email"}
+                className={"basicInput"}
+                placeholder={""}
+                name={"email"}
+                handler={inputHandlerFunction}          
+            />
+
+            <InputText 
+                type={"password"}
+                className={"basicInput"}
+                placeholder={""}
+                name={"password"}
+                handler={inputHandlerFunction}          
+            />
         </div>
     )
 }
