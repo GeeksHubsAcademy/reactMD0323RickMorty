e529b13962d97a9ab8526ea4473be76f8930fdf6
Dave86Dev <davidochando1986@gmail.com>
2023-05-05 12:41:18 +0200

    Data coming from the API perfectly, maping clickable elements

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,19 @@
 import React from 'react';
 import './Header.css';
 
+import {useNavigate} from 'react-router-dom';
+
 export const Header = () => {
+
+    //Instancio useNavigate para poder navegar..
+
+    const navigate = useNavigate();
 
     return (
         <div className='headerDesign'>
-
+            <div className='link' onClick={()=>navigate('/')}>home</div>
+            <div className='link' onClick={()=>navigate('/login')}>login</div>
+            <div className='link' onClick={()=>navigate('/register')}>register</div>
         </div>
     )
 }
--- src/layouts/Home/Home.jsx
+++ src/layouts/Home/Home.jsx
@@ -1,12 +1,58 @@
-
-import React from 'react';
-import './Home.css';
+import React, { useState, useEffect } from "react";
+import "./Home.css";
+import { bringCharacters } from "../../services/apiCalls";
+import { CharacterCard } from "../../components/CharacterCard/CharacterCard";
 
 export const Home = () => {
+  const [characters, setCharacters] = useState([]);
 
-    return(
-        <div className='homeDesign'>
-            soy Home
+  //useEffect para cuando el componente se está montando POR PRIMERA VEZ
+
+  // useEffect(()=>{
+
+  // },[]);
+
+  /*useEffect para cuando el componente se actualiza (se ejecutará SIEMPRE que haya
+    algún cambio en algún hook...)*/
+
+  // useEffect(()=>{
+
+  // });
+
+  //Tenemos los useEffect de seguimiento, que se ejecutarán sólo cuando cambie un hook en concreto
+
+  useEffect(() => {
+    if (characters.length === 0) {
+      bringCharacters()
+        .then((resultado) => {
+          if (resultado.data.results.length > 0) {
+            setCharacters(resultado.data.results);
+          }
+        })
+        .catch((error) => console.log(error));
+    }
+  }, [characters]);
+
+  return (
+    <div className="homeDesign">
+      {characters.length > 0 && (
+        /*aqui mapeare a los personajes porque ya han venido de la API
+        y por eso hemos entrado en este condicional*/
+
+        <div className="charactersDesign">
+          {characters.map((person) => {
+            return <div key={person.id} onClick={()=>console.log(person)}>
+                <CharacterCard 
+                    name={person.name}
+                    status={person.status}
+                    species={person.species}
+                    gender={person.gender}
+                    image={person.image}      
+                />
+            </div>;
+          })}
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
